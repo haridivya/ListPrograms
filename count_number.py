8119344a68e9c7cecd2b9f187a2015ffcdbc96f0
haridivya <123063502+haridivya@users.noreply.github.com>
2024-09-21 23:33:14 +0530

    Create count_number.py

---
+++ count_number.py
@@ -0,0 +1,18 @@
+'''
+Write a program to count the number of times the given value is repeated.
+Input Format:
+First line of input consists of our list elements.
+Second line of input consists of value to count.
+Output Format:
+Print thr number of times the given value is repeated in the list.
+Sample Input:
+10 20 10 40 10
+10
+Sample Output:
+3
+9)Write a program to print the given list in reverse order.
+Sample Input:
+10 20 30 40 50
+Sample Output:
+50 40 30 20 10 
+'''
