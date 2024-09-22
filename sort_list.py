57199199140e8dbef99a0437433a479f13c55564
haridivya <123063502+haridivya@users.noreply.github.com>
2024-09-22 12:33:05 +0530

    Update sort_list.py

--- sort_list.py
+++ sort_list.py
@@ -15,3 +15,21 @@ number=input().split(' ')
 empty_list=[int(num) for num in number]
 print(sort_list(empty_list))
 
+#without using sort function
+def sort_list(empty_list):
+    list1=[]
+    i,j,len_number=0,0,0
+    after_sort=''
+    while i==0:
+        j=min(empty_list)
+        list1.append(j)
+        empty_list.remove(j)
+        len_number=len(empty_list)
+        if len_number==0:
+            i+=1
+    for i in list1:
+        after_sort+=str(i)+' '
+    return after_sort
+number=input().split(' ')
+empty_list=[int(num) for num in number]
+print(sort_list(empty_list))
