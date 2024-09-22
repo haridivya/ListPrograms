05616e887e0f63ba1e5bfa8e777d0241771397f3
haridivya <123063502+haridivya@users.noreply.github.com>
2024-09-22 12:44:38 +0530

    Update reverse_list.py

--- reverse_list.py
+++ reverse_list.py
@@ -5,3 +5,23 @@ Sample Input:
 Sample Output:
 50 40 30 20 10 
 '''
+#using with reverse_function
+def reverse_number(number):
+    temp=''
+    number.reverse()
+    for i in number:
+        temp+=str(i)+' '
+    print(temp)
+number1=input().split(' ')
+number=[int(i) for i in number1]
+reverse_number(number)
+#without using the reverse method
+def reverse_num_without_using_reverse(number):
+    temp=''
+    temp_list=number[::-1]
+    for i in temp_list:
+        temp+=str(i)+' '
+    return temp
+number1=input().split(' ')
+number=[int(i) for i in number1]
+print(reverse_num_without_using_reverse(number))    
