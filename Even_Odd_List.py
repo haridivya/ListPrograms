04dbaf7034b2f84abf000aa3e260a5882363d5a1
haridivya <123063502+haridivya@users.noreply.github.com>
2024-09-22 20:22:02 +0530

    Update Even_Odd_List.py

--- Even_Odd_List.py
+++ Even_Odd_List.py
@@ -19,3 +19,16 @@ Sample Output:
 The even list [2, 6]
 The odd list [1, 3, 5]
 '''
+size_list=int(input())
+list1=[]
+for i in range(size_list):# it is used for input the values into list
+    list1.append(int(input()))
+even_list=[]
+odd_list=[]
+for i in list1:#it is used to check wheather given value is even or odd
+    if i%2==0:
+        even_list.append(i)
+    else:
+        odd_list.append(i)
+print(f'The even list {even_list}')
+print(f'The odd list {odd_list}')
